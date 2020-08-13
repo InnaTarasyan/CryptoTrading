3e13b9157c87431c49a23e394734558fbdc0356a
Inna Tarasyan <innatarasyanmail@gmail.com>
2020-08-13 17:06:26 +0400

    Appended Coindar Social Networks data

--- app/Http/Controllers/CoindarSocialsController.php
+++ app/Http/Controllers/CoindarSocialsController.php
@@ -29,55 +29,55 @@ class CoindarSocialsController extends Controller
 
         return Datatables::of($coins)
             ->editColumn('website', function ($coin){
-                if(!isset($coin->website)){
+                if(empty($coin->website)){
                     return 'Not Set';
                 }
                 return '<a href="'.$coin->website.'">'.$coin->website.'</a>';
             })
             ->editColumn('bitcointalk', function ($coin){
-                if(!isset($coin->bitcointalk)){
+                if(empty($coin->bitcointalk)){
                     return 'Not Set';
                 }
                 return '<a href="'.$coin->bitcointalk.'">'.$coin->bitcointalk.'</a>';
             })
             ->editColumn('twitter', function ($coin){
-                if(!isset($coin->twitter)){
+                if(empty($coin->twitter)){
                     return 'Not Set';
                 }
                 return '<a href="'.$coin->twitter.'">'.$coin->twitter.'</a>';
             })
             ->editColumn('reddit', function ($coin){
-                if(!isset($coin->reddit)){
+                if(empty($coin->reddit)){
                     return 'Not Set';
                 }
                 return '<a href="'.$coin->reddit.'">'.$coin->reddit.'</a>';
             })
             ->editColumn('telegram', function ($coin){
-                if(!isset($coin->telegram)){
+                if(empty($coin->telegram)){
                     return 'Not Set';
                 }
                 return '<a href="'.$coin->telegram.'">'.$coin->telegram.'</a>';
             })
             ->editColumn('facebook', function ($coin){
-                if(!isset($coin->facebook)){
+                if(empty($coin->facebook)){
                     return 'Not Set';
                 }
                 return '<a href="'.$coin->facebook.'">'.$coin->facebook.'</a>';
             })
             ->editColumn('github', function ($coin){
-                if(!isset($coin->github)){
+                if(empty($coin->github)){
                     return 'Not Set';
                 }
                 return '<a href="'.$coin->github.'">'.$coin->github.'</a>';
             })
             ->editColumn('explorer', function ($coin){
-                if(!isset($coin->github)){
+                if(empty($coin->github)){
                     return 'Not Set';
                 }
                 return '<a href="'.$coin->explorer.'">'.$coin->explorer.'</a>';
             })
             ->editColumn('youtube', function ($coin){
-                if(!isset($coin->youtube)){
+                if(empty($coin->youtube)){
                     return 'Not Set';
                 }
                 return '<a href="'.$coin->youtube.'">'.$coin->youtube.'</a>';
