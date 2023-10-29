c6257f8d8ee9fb26ae85321e78e6fc2c519c8caa
Inna Tarasyan <innatarasyanmail@gmail.com>
2023-10-29 23:44:41 +0400

    added model ohlcv_latests

--- app/Models/OhlcvLatest.php
+++ app/Models/OhlcvLatest.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class OhlcvHistory extends Model
+class OhlcvLatest extends Model
 {
     use HasFactory;
 
