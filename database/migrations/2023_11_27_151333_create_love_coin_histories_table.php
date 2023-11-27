315b434f72dabfe5dcf12219c54895613909933c
Inna Tarasyan <innatarasyanmail@gmail.com>
2023-11-27 19:37:16 +0400

    fix

--- database/migrations/2023_11_27_151333_create_love_coin_histories_table.php
+++ database/migrations/2023_11_27_151333_create_love_coin_histories_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('symbol')->nullable();
             $table->integer('rank');
-            $table->integer('age');
+            $table->integer('age')->nullable();
             $table->string('color');
             $table->string('png32')->nullable();
             $table->string('png64')->nullable();
