e42aa7ca0c203eeafc90a8ded03a2a71a348fd3f
Inna Tarasyan <innatarasyanmail@gmail.com>
2025-07-20 16:26:23 +0400

    "Derivatives" page, design enhancements

--- public/js/coingecko/derivatives.js
+++ public/js/coingecko/derivatives.js
@@ -32,6 +32,65 @@ Derivatives.prototype.init = function () {
             });
         }
     });
+
+    // Replace default DataTables filter with custom search bar (pink gradient)
+    const filter = $('.dataTables_filter');
+    const customSearch = `
+        <div class="search-wrapper">
+            <svg class="search-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="11" cy="11" r="7" stroke="#ff6a88" stroke-width="2"/><line x1="16.018" y1="16.4853" x2="21" y2="21.4673" stroke="#ff6a88" stroke-width="2" stroke-linecap="round"/></svg>
+            <input type="search" class="form-control" placeholder="Search derivatives..." aria-controls="coingecko_derivatives" style="padding-left:44px;" />
+            <button id="clear-search" class="ml-2" type="button">Clear</button>
+        </div>
+    `;
+    filter.html(customSearch);
+
+    // Bind search input
+    const searchBox = filter.find('input[type="search"]');
+    searchBox.on('input', function() {
+        oTable.search(searchBox.val()).draw();
+    });
+
+    // Bind clear button
+    filter.on('click', '#clear-search', function() {
+        searchBox.val('');
+        oTable.search('').draw();
+    });
+
+    // ======================== Highlight Search Results ========================
+    function highlightSearchResults() {
+        var table = $('#coingecko_derivatives').DataTable();
+        var searchTerm = table.search();
+        if (!searchTerm) {
+            $('#coingecko_derivatives tbody td').each(function() {
+                $(this).html($(this).text());
+            });
+            return;
+        }
+        var regex = new RegExp('('+searchTerm.replace(/[.*+?^${}()|[\\]\]/g, '\\$&')+')', 'gi');
+        $('#coingecko_derivatives tbody tr').each(function() {
+            var row = $(this);
+            var found = false;
+            row.find('td').each(function(idx) {
+                var cell = $(this);
+                var original = cell.text();
+                if (searchTerm && original.match(regex)) {
+                    var newHtml = original.replace(regex, '<span class="highlight">$1</span>');
+                    cell.html(newHtml);
+                    found = true;
+                } else {
+                    cell.html(original);
+                }
+            });
+            if (found) {
+                row.addClass('highlight-row');
+            } else {
+                row.removeClass('highlight-row');
+            }
+        });
+    }
+    oTable.on('draw', highlightSearchResults);
+    searchBox.on('input', highlightSearchResults);
+    filter.on('click', '#clear-search', highlightSearchResults);
 };
 
 Derivatives.prototype.bindEvents = function () {
--- resources/views/coingecko/derivatives.blade.php
+++ resources/views/coingecko/derivatives.blade.php
@@ -1,96 +1,72 @@
 @extends('layouts.base')
 @section('styles')
     <link href="https://cdn.datatables.net/1.10.12/css/jquery.dataTables.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/buttons/2.4.1/css/buttons.dataTables.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/responsive/2.5.0/css/responsive.dataTables.min.css" rel="stylesheet">
     <link href="{{url('css/datatables.css')}}" rel="stylesheet">
+    <link href="{{ asset('css/derivatives.css') }}" rel="stylesheet">
 @endsection
 @section('content')
     <div class="m-content">
-
-
-        <ul class="m-subheader__breadcrumbs m-nav m-nav--inline">
-            <li class="m-nav__item">
-                <a href="/coingeckomarketsindex" class="m-nav__link">
-                        <span class="m-nav__link-text">
-                           Markets
-                        </span>
-                </a>
-            </li>
-            <li class="m-nav__item">
-                <a href="/coingeckoexchangesindex" class="m-nav__link">
-                        <span class="m-nav__link-text">
-                           Exchanges
-                        </span>
-                </a>
-            </li>
-            <li class="m-nav__item">
-                <a href="/coingeckotrendingsindex" class="m-nav__link">
-                        <span class="m-nav__link-text">
-                           Trendings
-                        </span>
-                </a>
-            </li>
-            <li class="m-nav__item">
-                <a href="/coingeckoexchangeratesindex" class="m-nav__link">
-                        <span class="m-nav__link-text">
-                           Exchange Rates
-                        </span>
-                </a>
-            </li>
-            <li class="m-nav__item">
-                <a href="/coingeckonftsindex" class="m-nav__link">
-                        <span class="m-nav__link-text">
-                            Nfts
-                        </span>
-                </a>
-            </li>
-            <li class="m-nav__item">
-                <a href="/coingeckoderivativesindex" class="m-nav__link">
-                        <span class="m-nav__link-text">
-                            Derivatives
-                        </span>
-                </a>
-            </li>
-            <li class="m-nav__item">
-                <a href="/coingeckoderivativesexchangesindex" class="m-nav__link">
-                        <span class="m-nav__link-text">
-                            Derivatives Exchanges
-                        </span>
-                </a>
-            </li>
-        </ul>
-
-        <!--Begin::Section-->
-        <div class="m-portlet" >
-            <div class="m-portlet__body  m-portlet__body--no-padding">
+        <!-- Modern Title Bar with Icon -->
+        <div class="modern-title-bar" aria-labelledby="derivativesTitle" role="banner">
+            <div class="modern-title-bar-row">
+                <div class="modern-title-main" style="display: flex; align-items: center; gap: 1em;">
+                    <span class="modern-title-icon" tabindex="0" title="Derivatives Overview">
+                        <!-- Derivatives Icon SVG (Pink Gradient) -->
+                        <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <defs>
+                                <linearGradient id="derivativesGradient" x1="0" y1="0" x2="32" y2="32" gradientUnits="userSpaceOnUse">
+                                    <stop stop-color="#ff6a88"/>
+                                    <stop offset="1" stop-color="#ff99ac"/>
+                                </linearGradient>
+                            </defs>
+                            <rect x="4" y="4" width="24" height="24" rx="8" fill="url(#derivativesGradient)"/>
+                            <path d="M10 16h12M16 10v12" stroke="#fff" stroke-width="2.5" stroke-linecap="round"/>
+                        </svg>
+                    </span>
+                    <span class="modern-title-text" id="derivativesTitle">Coingecko Derivatives</span>
+                </div>
+            </div>
+        </div>
+        <!-- DataTable Section -->
+        <div class="m-portlet enhanced-portlet">
+            <div class="m-portlet__body mt-5 enhanced-portlet-body">
                 <input type="hidden" id="coingecko_derivatives_route" value="{{ route('datatable.coingecko.derivatives') }}">
-                <div class="table-responsive">
-                    <table id="coingecko_derivatives" class="table table-hover table-condensed table-striped" style="width:100%; padding-top:1%">
-                        <thead>
+                <div class="enhanced-table-container table-responsive">
+                    <table id="coingecko_derivatives" class="table table-hover table-condensed table-striped enhanced-table" style="width:100%; padding-top:1%">
+                        <thead class="enhanced-thead">
                         <tr>
-                            <th>Market</th>
-                            <th>Index Id</th>
-                            <th>Price</th>
-                            <th>Price percentage change 24h</th>
-                            <th>Contract type</th>
-                            <th>Index</th>
-                            <th>Basis</th>
-                            <th>Spread</th>
-                            <th>Funding rate</th>
-                            <th>Open interest</th>
-                            <th>Volume 24h</th>
-                            <th>Last traded at</th>
-                            <th>Expired at</th>
+                            <th class="enhanced-th">Market</th>
+                            <th class="enhanced-th">Index Id</th>
+                            <th class="enhanced-th">Price</th>
+                            <th class="enhanced-th">Price % Change 24h</th>
+                            <th class="enhanced-th">Contract Type</th>
+                            <th class="enhanced-th">Index</th>
+                            <th class="enhanced-th">Basis</th>
+                            <th class="enhanced-th">Spread</th>
+                            <th class="enhanced-th">Funding Rate</th>
+                            <th class="enhanced-th">Open Interest</th>
+                            <th class="enhanced-th">Volume 24h</th>
+                            <th class="enhanced-th">Last Traded At</th>
+                            <th class="enhanced-th">Expired At</th>
                         </tr>
                         </thead>
                     </table>
                 </div>
             </div>
         </div>
-
-        <!--End::Section-->
     </div>
 @endsection
 @section('scripts')
     <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.1/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.print.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.colVis.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.5.0/js/dataTables.responsive.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
     <script src="{{ url('js/coingecko/derivatives.js') }}"></script>
 @endsection
