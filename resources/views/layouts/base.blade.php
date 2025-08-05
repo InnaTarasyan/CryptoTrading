6baf092f4ff8fbb71d363390889d5bfce83162ac
Inna Tarasyan <innatarasyanmail@gmail.com>
2025-08-05 17:33:11 +0400

    added meta tags

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -3,17 +3,134 @@
 <!-- begin::Head -->
 <head>
     <meta charset="utf-8" />
+    
+    <!-- Primary Meta Tags -->
     <title>
-        Crypto Trading
+        @yield('title', 'Crypto Trading - Live Cryptocurrency Tracking & Analysis Platform')
     </title>
-    <meta name="description" content="My Coin Trading Project allows online tracking of the changes in key indicators of the Crypto-currency,
-                                including the Social Volume Changes in the Reddit and Twitter, etc. ">
-    <meta name="keywords" content="Coins, CryptoCoins, worldcoinindex.com, coindar.org, solume.io, coinmarketcap.org, TradingView Charts, Twitter, CoinPais, CryptoCurrency, Reddit">
+    <meta name="title" content="@yield('meta_title', 'Crypto Trading - Live Cryptocurrency Tracking & Analysis Platform')">
+    <meta name="description" content="@yield('meta_description', 'Track live cryptocurrency prices, social volume changes on Reddit and Twitter, and comprehensive crypto market analysis. Real-time data from CoinMarketCap, TradingView, and more.')">
+    <meta name="keywords" content="@yield('meta_keywords', 'cryptocurrency, bitcoin, ethereum, crypto trading, live crypto prices, cryptocurrency tracking, crypto market analysis, bitcoin price, ethereum price, altcoins, crypto portfolio, trading view, coinmarketcap, crypto news, blockchain, digital currency, crypto investment, crypto signals, crypto charts, crypto trading platform')">
     <meta name="author" content="Inna Tarasyan">
+    <meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1">
+    <meta name="language" content="English">
+    <meta name="revisit-after" content="7 days">
+    <meta name="rating" content="general">
+    <meta name="distribution" content="global">
+    <meta name="coverage" content="worldwide">
+    <meta name="target" content="all">
+    <meta name="HandheldFriendly" content="true">
+    <meta name="MobileOptimized" content="width">
+    <meta name="format-detection" content="telephone=no">
+    
+    <!-- Canonical URL -->
+    <link rel="canonical" href="{{ url()->current() }}" />
+    
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="{{ url()->current() }}">
+    <meta property="og:title" content="@yield('og_title', 'Crypto Trading - Live Cryptocurrency Tracking & Analysis Platform')">
+    <meta property="og:description" content="@yield('og_description', 'Track live cryptocurrency prices, social volume changes on Reddit and Twitter, and comprehensive crypto market analysis. Real-time data from CoinMarketCap, TradingView, and more.')">
+    <meta property="og:image" content="@yield('og_image', url('assets/demo/demo7/media/img/logo/logo.png'))">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:image:height" content="630">
+    <meta property="og:image:alt" content="Crypto Trading Platform Logo">
+    <meta property="og:site_name" content="Crypto Trading">
+    <meta property="og:locale" content="en_US">
+    <meta property="og:locale:alternate" content="ru_RU">
+    <meta property="og:locale:alternate" content="hy_AM">
+    <meta property="og:locale:alternate" content="fi_FI">
+    
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="{{ url()->current() }}">
+    <meta property="twitter:title" content="@yield('twitter_title', 'Crypto Trading - Live Cryptocurrency Tracking & Analysis Platform')">
+    <meta property="twitter:description" content="@yield('twitter_description', 'Track live cryptocurrency prices, social volume changes on Reddit and Twitter, and comprehensive crypto market analysis. Real-time data from CoinMarketCap, TradingView, and more.')">
+    <meta property="twitter:image" content="@yield('twitter_image', url('assets/demo/demo7/media/img/logo/logo.png'))">
+    <meta property="twitter:site" content="@cryptotrading">
+    <meta property="twitter:creator" content="@cryptotrading">
+    
+    <!-- Additional SEO Meta Tags -->
+    <meta name="application-name" content="Crypto Trading">
+    <meta name="apple-mobile-web-app-title" content="Crypto Trading">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="default">
+    <meta name="msapplication-TileColor" content="#667eea">
+    <meta name="msapplication-TileImage" content="{{ url('assets/demo/demo7/media/img/logo/favicon.ico') }}">
+    <meta name="msapplication-config" content="{{ url('browserconfig.xml') }}">
+    <meta name="theme-color" content="#667eea">
+    <meta name="color-scheme" content="light dark">
+    
+    <!-- Viewport and Compatibility -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no">
     <meta name="_token" content="{{ csrf_token() }}">
-    <!--begin::Web font -->
+    
+    <!-- DNS Prefetch and Preconnect for Performance -->
+    <link rel="dns-prefetch" href="//ajax.googleapis.com">
+    <link rel="dns-prefetch" href="//fonts.googleapis.com">
+    <link rel="dns-prefetch" href="//www.google-analytics.com">
+    <link rel="dns-prefetch" href="//www.googletagmanager.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    
+    <!-- Favicon and App Icons -->
+    <link rel="shortcut icon" href="{{ url('assets/demo/demo7/media/img/logo/favicon.ico') }}" />
+    <link rel="icon" type="image/x-icon" href="{{ url('assets/demo/default/media/img/logo/favicon.ico') }}" />
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ url('assets/demo/demo7/media/img/logo/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ url('assets/demo/demo7/media/img/logo/favicon-16x16.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ url('assets/demo/demo7/media/img/logo/apple-touch-icon.png') }}">
+    <link rel="manifest" href="{{ url('site.webmanifest') }}">
+    
+    <!-- Structured Data / JSON-LD -->
+    <script type="application/ld+json">
+    {
+        "@context": "https://schema.org",
+        "@type": "WebSite",
+        "name": "Crypto Trading",
+        "url": "{{ url('/') }}",
+        "description": "Track live cryptocurrency prices, social volume changes on Reddit and Twitter, and comprehensive crypto market analysis.",
+        "potentialAction": {
+            "@type": "SearchAction",
+            "target": "{{ url('/') }}?q={search_term_string}",
+            "query-input": "required name=search_term_string"
+        },
+        "publisher": {
+            "@type": "Organization",
+            "name": "Crypto Trading",
+            "logo": {
+                "@type": "ImageObject",
+                "url": "{{ url('assets/demo/demo7/media/img/logo/logo.png') }}"
+            }
+        },
+        "sameAs": [
+            "https://twitter.com/cryptotrading",
+            "https://t.me/cryptotrading"
+        ]
+    }
+    </script>
+    
+    <script type="application/ld+json">
+    {
+        "@context": "https://schema.org",
+        "@type": "Organization",
+        "name": "Crypto Trading",
+        "url": "{{ url('/') }}",
+        "logo": "{{ url('assets/demo/demo7/media/img/logo/logo.png') }}",
+        "description": "Live cryptocurrency tracking and analysis platform",
+        "address": {
+            "@type": "PostalAddress",
+            "addressCountry": "US"
+        },
+        "contactPoint": {
+            "@type": "ContactPoint",
+            "contactType": "customer service",
+            "availableLanguage": ["English", "Russian", "Armenian", "Finnish"]
+        }
+    }
+    </script>
+    
+    <!-- begin::Web font -->
     <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js"></script>
     <script>
         WebFont.load({
@@ -24,6 +141,7 @@
         });
     </script>
     <!--end::Web font -->
+    
     <!--begin::Base Styles -->
     <!--begin::Page Vendors -->
     <link href="{{url('assets/vendors/custom/fullcalendar/fullcalendar.bundle.css') }}" rel="stylesheet" type="text/css" />
@@ -31,9 +149,13 @@
     <link href="{{url('assets/vendors/base/vendors.bundle.css')}}" rel="stylesheet" type="text/css" />
     <link href="{{url('assets/demo/demo7/base/style.bundle.css')}}" rel="stylesheet" type="text/css" />
     <!--end::Base Styles -->
-    <link rel="shortcut icon" href="{{ url('assets/demo/demo7/media/img/logo/favicon.ico') }}" />
-    <link rel="icon" type="image/x-icon" href="{{ url('assets/demo/default/media/img/logo/favicon.ico') }}" />
     <link href="{{url('css/language-switcher.css')}}" rel="stylesheet" type="text/css" />
+    
+    <!-- Preload Critical Resources -->
+    <link rel="preload" href="{{url('assets/vendors/base/vendors.bundle.css')}}" as="style">
+    <link rel="preload" href="{{url('assets/demo/demo7/base/style.bundle.css')}}" as="style">
+    <link rel="preload" href="{{url('assets/vendors/base/vendors.bundle.js')}}" as="script">
+    
     @yield('styles')
 </head>
 <!-- end::Head -->
