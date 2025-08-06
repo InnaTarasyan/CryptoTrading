d22b6bfe58c2d65c82f5d76f9a59d1955d68cd8f
Inna Tarasyan <innatarasyanmail@gmail.com>
2025-08-06 12:43:00 +0400

    yandex metrica

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -81,8 +81,24 @@
     <link rel="icon" type="image/png" sizes="16x16" href="{{ url('assets/demo/demo7/media/img/logo/favicon-16x16.png') }}">
     <link rel="apple-touch-icon" sizes="180x180" href="{{ url('assets/demo/demo7/media/img/logo/apple-touch-icon.png') }}">
     <link rel="manifest" href="{{ url('site.webmanifest') }}">
-    
-    <!-- Structured Data / JSON-LD -->
+
+
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript">
+        (function(m,e,t,r,i,k,a){
+            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+        })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=103614600', 'ym');
+
+        ym(103614600, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/103614600" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -
+
+
+        <!-- Structured Data / JSON-LD -->
     <script type="application/ld+json">
     {
         "@context": "https://schema.org",
